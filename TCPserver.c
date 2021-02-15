76158361478f8b0f485db6619a61eff0de3a2f03
unknown <cathyyyychoi@gmail.com>
2021-02-15 13:24:09 +0900

    update TCP C source file

--- TCPserver.c
+++ TCPserver.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -6,20 +5,19 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1024
 #define on_error(...) { fprintf(stderr, __VA_ARGS__); fflush(stderr); exit(1); }
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) on_error("Usage: %s [port]\n", argv[0]);
+    if (argc < 2) on_error("Usage: %s [port]\n", argv[0]); // input: ./TCPserver port-num
 
     int port = atoi(argv[1]);
 
     int server_fd, client_fd, err;
     struct sockaddr_in server, client;
-    char buf[BUFFER_SIZE];
+    char buf[1024];
     char temp[20];
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0); // create socket
     if (server_fd < 0) on_error("Could not create socket\n");
 
     server.sin_family = AF_INET;
@@ -29,7 +27,7 @@ int main(int argc, char* argv[]) {
     int opt_val = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof opt_val);
 
-    err = bind(server_fd, (struct sockaddr*)&server, sizeof(server));
+    err = bind(server_fd, (struct sockaddr*)&server, sizeof(server)); // binding
     if (err < 0) on_error("Could not bind socket\n");
 
     err = listen(server_fd, 128);
@@ -40,22 +38,24 @@ int main(int argc, char* argv[]) {
     while (1) {
         socklen_t client_len = sizeof(client);
         client_fd = accept(server_fd, (struct sockaddr*)&client, &client_len);
+
         inet_ntop(AF_INET, &client.sin_addr.s_addr, temp, sizeof(temp));
         printf("Server : %s client connected \n", temp);
 
         if (client_fd < 0) on_error("Could not establish new connection\n");
 
         while (1) {
-            int read = recv(client_fd, buf, BUFFER_SIZE, 0);
+            int read = recv(client_fd, buf, 1024, 0);
+            printf("Received message: %s\n", buf); // print received message from client
 
-            if (!read) break; // done reading
+            if (!read) break; // reading end
             if (read < 0) on_error("Client read failed\n");
 
             err = send(client_fd, buf, read, 0);
             if (err < 0) on_error("Client write failed\n");
         }
-     
-        printf("Server : %s client closed \n", temp);
+
+        printf("Server : %s client closed \n", temp); // connection end
     }
 
 
